6b1e5323e24ada81c6768a441c7f9ea2238c94fc
dan-wahlin-strawberry <dan.wahlin@strawberry.se>
2024-06-07 13:00:00 +0200

    feat: extend localization

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -22,25 +22,28 @@ const SignupPage: React.FC<SignupPageProps> = ({ user, setUser }) => {
     setLanguage(e.target.value as Language)
   }, [])
 
+  const labels = getLocalizedLabels(language)
+
   return (
     <div style={{ padding: '2rem', width: '100%' }}>
       <main>
         <h1>Company</h1>
         <div style={{ display: 'flex', width: '100%' }}>
           <div style={{ width: '100%', paddingRight: '2rem' }}>
-            <h2>Signup</h2>
+            <h2>{labels.title}</h2>
             <Form setUser={setUser} language={language} />
           </div>
           <div style={{ width: '100%' }}>
-            <h2>You should join us because:</h2>
-            <p>* We are a nice company</p>
-            <p>* Free breakfast!</p>
+            <h2>{labels.aboutTitle}</h2>
+            {labels.perks.map((perk) => (
+              <p key={perk}>* {perk}</p>
+            ))}
           </div>
         </div>
-        Already have an account? <Link to={`/login`}>Login</Link>
+        {labels.login.text} <Link to={`/login`}>{labels.login.cta}</Link>
       </main>
       <div style={{ marginTop: '5rem' }}>
-        <p>Language:</p>
+        <p>{labels.language}</p>
         <Select
           id="language-picker"
           value={language}
@@ -57,3 +60,32 @@ const SignupPage: React.FC<SignupPageProps> = ({ user, setUser }) => {
 }
 
 export default SignupPage
+
+const getLocalizedLabels = (
+  language: Language,
+): {
+  aboutTitle: string
+  perks: string[]
+  language: string
+  login: { text: string; cta: string }
+  title: string
+} => {
+  switch (language) {
+    case 'EN':
+      return {
+        aboutTitle: 'You should just us because:',
+        perks: ['We are a nice company', 'Free breakfast!'],
+        language: 'Language',
+        login: { text: 'Already have an account?', cta: 'Login' },
+        title: 'Become a member',
+      }
+    case 'SV':
+      return {
+        aboutTitle: 'Du borde gå med oss för att:',
+        perks: ['Vi är ett bra företag', 'Gratis frukost!'],
+        language: 'Språk',
+        login: { text: 'Har du redan ett konto?', cta: 'Logga in' },
+        title: 'Bli medlem',
+      }
+  }
+}
